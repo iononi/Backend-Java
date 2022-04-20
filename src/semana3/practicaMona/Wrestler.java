86bfaf8ed7deaa43b6945e885c42ee03218afc22
Eduardo Ruiz Rios <eduardo.ruiz.rios09@gmail.com>
2022-04-19 23:05:48 -0500

    add: Concrete class Wrestler

---
+++ src/semana3/practicaMona/Wrestler.java
@@ -0,0 +1,43 @@
+package semana3.practicaMona;
+
+public class Wrestler extends Mona {
+
+    private boolean haveMask;
+    private String suitColor;
+
+    public Wrestler() {
+        this("Wrestler", true, "Black");
+    }
+
+    public Wrestler(String hobby, boolean haveMask, String suitColor) {
+        super.setHOBBY(hobby);
+        this.haveMask = haveMask;
+        this.suitColor = suitColor;
+    }
+
+    public boolean haveMask() {
+        return haveMask;
+    }
+
+    public void setHaveMask(boolean haveMask) {
+        this.haveMask = haveMask;
+    }
+
+    public String getSuitColor() {
+        return suitColor;
+    }
+
+    public void setSuitColor(String suitColor) {
+        this.suitColor = suitColor;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + String.format("and %s", activity());
+    }
+
+    @Override
+    public String activity() {
+        return "I like wrestling!";
+    }
+}
