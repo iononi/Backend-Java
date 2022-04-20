8a6962ddeaeee228fdfbf940e2a66e0338327877
Eduardo Ruiz Rios <eduardo.ruiz.rios09@gmail.com>
2022-04-19 23:04:34 -0500

    add: Abstract class to represetn Mona the Octocat

---
+++ src/semana3/practicaMona/Mona.java
@@ -0,0 +1,36 @@
+package semana3.practicaMona;
+
+public abstract class Mona {
+
+    private String HOBBY;
+    private final String NAME = "Mona";
+    private final String EYES_COLOR = "Light brown";
+
+    public Mona() {
+
+    }
+
+    public void setHOBBY(String hobby) {
+        HOBBY = hobby;
+    }
+
+    public String getHOBBY() {
+        return HOBBY;
+    }
+
+    public String getNAME() {
+        return NAME;
+    }
+
+    public String getEYES_COLOR() {
+        return EYES_COLOR;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Hi! My name is %s, I have %s eyes. My hobby is %s ", NAME, EYES_COLOR,
+                HOBBY);
+    }
+
+    public abstract String activity();
+}
