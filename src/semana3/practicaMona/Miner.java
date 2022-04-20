917114fe4492bc09f6f269759757f9bb5867b070
Eduardo Ruiz Rios <eduardo.ruiz.rios09@gmail.com>
2022-04-19 23:04:07 -0500

    add: Concrete class Miner

---
+++ src/semana3/practicaMona/Miner.java
@@ -0,0 +1,22 @@
+package semana3.practicaMona;
+
+public class Miner extends Mona {
+
+    public Miner() {
+        this("Miner");
+    }
+
+    public Miner(String hobby) {
+        super.setHOBBY(hobby);
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + String.format("and %s", activity());
+    }
+
+    @Override
+    public String activity() {
+        return "the best part of mining is when you find gold!";
+    }
+}
