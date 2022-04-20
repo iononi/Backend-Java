eee2711d9f7d058710720c3d0644c54583ce8095
Eduardo Ruiz Rios <eduardo.ruiz.rios09@gmail.com>
2022-04-19 23:04:58 -0500

    add: Concrete class Scubatocat

---
+++ src/semana3/practicaMona/Scubatocat.java
@@ -0,0 +1,23 @@
+package semana3.practicaMona;
+
+public class Scubatocat extends Mona {
+
+
+    public Scubatocat() {
+        this("Diving");
+    }
+
+    public Scubatocat(String hobby) {
+        super.setHOBBY(hobby);
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + String.format("and %s", activity());
+    }
+
+    @Override
+    public String activity(){
+        return "I like the sea!";
+    }
+}
