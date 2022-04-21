f45b691f14954ef5f0f41ea93016bc69b949342d
Eduardo Ruiz Rios <eduardo.ruiz.rios09@gmail.com>
2022-04-21 17:56:20 -0500

    add: Interface to declare what spidermen can do

---
+++ src/semana3/practicaMultiverse/SpiderPowers.java
@@ -0,0 +1,10 @@
+package semana3.practicaMultiverse;
+
+import imonsh.Screen;
+
+public interface SpiderPowers {
+    void webSwinging(Screen s);
+    void wallCrawling(Screen s);
+    void spiderSense(Screen s);
+    void showAbilities(Screen s);
+}
