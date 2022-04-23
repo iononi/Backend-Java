59591be1d001cdcd72b9255b89facfae74141793
Eduardo Ruiz Rios <eduardo.ruiz.rios09@gmail.com>
2022-04-23 15:54:00 -0500

    add: main test class

--- src/semana3/practicaMultiverse/SpiderManTest.java
+++ src/semana3/practicaMultiverse/SpiderManTest.java
@@ -21,7 +21,7 @@ public class SpiderManTest {
                             for (SpiderPowers spidey : spideys) {
                                 spidey.showAbilities(screen);
                                 try {
-                                    Thread.sleep(10000);
+                                    Thread.sleep(5000);
                                 } catch (InterruptedException e) {
                                     e.printStackTrace();
                                 }
